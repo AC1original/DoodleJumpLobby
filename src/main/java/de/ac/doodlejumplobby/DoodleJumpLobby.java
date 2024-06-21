c26de002c40f1a83e9317d3e0331cae25e76543a
ac1 <ac1original.lol@gmail.com>
2024-06-21 09:18:39 +0200

    build project

---
+++ src/main/java/de/ac/doodlejumplobby/DoodleJumpLobby.java
@@ -0,0 +1,17 @@
+package de.ac.doodlejumplobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DoodleJumpLobby extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
